6998d1e342234aaeafaf28399ed62849fa82d518
Alexandre Melo <xandymelo@outlook.com.br>
2021-07-08 19:14:56 -0300

    Create radiacaoglobal.py

---
+++ radiacaoglobal.py
@@ -0,0 +1,26 @@
+import json
+from datetime import date
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    data = date.today()
+    client = boto3.resource("dynamodb")
+    table = client.Table("DadosAPI")
+    dados = table.scan()['Items']
+    pont = False
+    for dado in dados:
+        if dado['Data'] == str(data):
+            pont = True
+    if pont == False:
+        return"Ainda não temos dados para data de hoje!"
+    else:
+        #pegar os dados do dynamo
+        respo = table.get_item(
+        Key={
+            'Data': str(data)
+            }
+        )
+        item = respo['Item']
+        radiacao_global = item['RADIACAO_GLOBAL']
+        return {'VALOR_OBSERVADO': radiacao_global}
